8a1bcce5710c9ea2900101e96eb25930b12e1874
Piriou SCOTT <pscott@student.42.fr>
2019-03-24 01:43:08 +0100

    Update gitignore ; working main example for signals

--- main.c
+++ main.c
@@ -2,16 +2,58 @@
 #include <stdio.h>
 #include <unistd.h>
 
+
 void	sig_handler(int signo)
 {
-	printf("Received signal num : %d\n", signo);
+	write(1, "Resetting\n", 10);
+	signal(SIGALRM, SIG_DFL);
+	signal(SIGVTALRM, SIG_DFL);
+	signal(SIGPROF, SIG_DFL);
+	signal(SIGTERM, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+	signal(SIGHUP, SIG_DFL);
+	signal(SIGFPE, SIG_DFL);
+	signal(SIGILL, SIG_DFL);
+	signal(SIGSEGV, SIG_DFL);
+	signal(SIGBUS, SIG_DFL);
+	signal(SIGABRT, SIG_DFL);
+	signal(SIGIOT, SIG_DFL);
+	signal(SIGTRAP, SIG_DFL);
+	signal(SIGEMT, SIG_DFL);
+	signal(SIGSYS, SIG_DFL);
+	raise(signo);
+}
+
+void	sigint_handler(int signo)
+{
+	(void)signo;
+	write(1, "\n", 1);
+}
+
+void	signal_setup(void)
+{
+	signal(SIGINT, sigint_handler);
+	signal(SIGALRM, sig_handler);
+	signal(SIGVTALRM, sig_handler);
+	signal(SIGPROF, sig_handler);
+	signal(SIGTERM, sig_handler);
+	signal(SIGQUIT, sig_handler);
+	signal(SIGHUP, sig_handler);
+	signal(SIGFPE, sig_handler);
+	signal(SIGILL, sig_handler);
+	signal(SIGSEGV, sig_handler);
+	signal(SIGBUS, sig_handler);
+	signal(SIGABRT, sig_handler);
+	signal(SIGIOT, sig_handler);
+	signal(SIGTRAP, sig_handler);
+	signal(SIGEMT, sig_handler);
+	signal(SIGSYS, sig_handler);
 }
 
 int	main(void)
 {
-	if (signal(SIGINT, sig_handler) == SIG_ERR)
-		printf("\nCan't catch SIGINT\n");
-	while (1)
-		sleep(1);
+	signal_setup();
+	while (42)
+		;
 	return (0);
 }
