e424aeb60d2470e0273b3aab1dd8fbcb66d855c8
Piriou SCOTT <pscott@student.42.fr>
2019-03-25 10:43:39 +0100

    Fixed a bug where moving with arrows would change list's start

--- srcs/decoration.c
+++ srcs/decoration.c
@@ -2,14 +2,17 @@
 
 int		move_right(t_arg_list *lst, t_print_info *info, char *direction)
 {
+	t_arg_list *tmp;
+
 	if (!lst)
 		return (0);
-	while (!lst->current)
-		lst = lst->next;
+	tmp = lst;
+	while (!tmp->current)
+		tmp = tmp->next;
 	if (ft_strncmp(direction, "right", 6) == 0)
 	{
-		lst->current = 0;
-		lst->next->current = 1;
+		tmp->current = 0;
+		tmp->next->current = 1;
 	}
 	print_list(lst, info);
 	return (1);
