d03df327b9e2d3ab9a421d6471861562c547c466
Piriou SCOTT <pscott@student.42.fr>
2019-04-01 17:25:51 +0200

    Fixed retrieve_pos function, now properly _draining_ read before getting cursor position

--- libterm/srcs/cursor.c
+++ libterm/srcs/cursor.c
@@ -12,29 +12,32 @@ static void	sanitize_pos_values(int *col, int *row)
 
 static void	parse_pos(char *pos_str, t_pos *curr_pos)
 {
-	char	*col_start;
+	char			*col_start;
 
+	
 	curr_pos->row = ft_atoi(pos_str + 2);
-	col_start = ft_strchr(pos_str, ';'); // error
-	curr_pos->col = col_start ? ft_atoi(col_start + 1) : 0;
+	col_start = ft_strchr(pos_str + 3, ';'); //TODO: error
+	curr_pos->col = col_start ? ft_atoi(col_start + 1) : 0; //TODO: error
 }
 
 static void	get_pos(char *pos_str)
 {
 	int	len;
 
-	tputs(GET_POS, 1, ft_putchar);//error tputs
-	if ((len = read(STDIN, pos_str, 20) > 0))//error read
+	while (ft_atoi(pos_str + 2) == 0)
+	{
+		tputs(GET_POS, 1, ft_putchar);
+		if ((len = read(STDIN, pos_str, 50)) < 0)
+			ft_printf("ERROR");//TODO: pls
 		pos_str[len] = 0;
-	else if (len == -1)
-		term_putstr_endline("error: failed to read", 2);
+	}
 }
 
 void	retrieve_pos(t_pos *curr_pos)
 {
-	char	pos_str[21];
+	char	pos_str[50];
 
-	ft_bzero(pos_str, 21);
+	ft_bzero(pos_str, 50);
 	get_pos(pos_str);
 	parse_pos(pos_str, curr_pos);
 }
