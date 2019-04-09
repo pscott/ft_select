dde3a3196a656a6d2317658ad5af10aca91203fc
Piriou SCOTT <pscott@student.42.fr>
2019-04-09 11:57:19 +0200

    Output an error msg if write to /dev/tty fails

--- libterm/srcs/ft_putchar_tty.c
+++ libterm/srcs/ft_putchar_tty.c
@@ -6,7 +6,7 @@
 /*   By: pscott <pscott@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/04 18:59:20 by pscott            #+#    #+#             */
-/*   Updated: 2019/04/04 18:59:21 by pscott           ###   ########.fr       */
+/*   Updated: 2019/04/06 10:15:29 by pscott           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 int		ft_putchar_tty(int c)
 {
 	if (write(g_dev_tty, &c, 1) == -1)
+	{
+		ft_putstr_fd("write error: bad file descriptor\n", 2);
 		return (0);
+	}
 	else
 		return (1);
 }
